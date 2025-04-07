ae42e95b5f162ed59e18dcde754e5ce452a83d24
Keith <keithwipf1@gmail.com>
2025-04-07 11:18:44 -0500

    Revert "CMake: add SPEECH_CORE_STATIC_SCREEN_READER_APIS option"
    This reverts commit 922fcd453d1ccca84a593fc411962beb03a1cb9f.
    
    Under the GPL licence, you can't actually do this legally without also making your app open source or providing object files.

--- src/SCDrivers/nvda.cpp
+++ src/SCDrivers/nvda.cpp
@@ -1,23 +1,17 @@
 #include "nvda.h"
 
 ScreenReaderNVDA::ScreenReaderNVDA() :
-	#ifndef SPEECH_CORE_STATIC_SCREEN_READER_APIS
 	ScreenReader(L"NVDA", SC_HAS_SPEECH),module(NULL),
 nvdaController_testIfRunning_fn(NULL),
 nvdaController_speakText_fn(NULL),
 nvdaController_cancelSpeech_fn(NULL),
 loaded(false), Is_Active(false) {
 }
-	#else
-	ScreenReader(L"NVDA", SC_HAS_SPEECH),
-loaded(false), Is_Active(false) {
-}
-	#endif
 ScreenReaderNVDA::~ScreenReaderNVDA() {
 	this->release();
 	}
 	void ScreenReaderNVDA::init() {
-		#ifndef SPEECH_CORE_STATIC_SCREEN_READER_APIS
+
 		if (!this->loaded) {
 			this->module = LoadLibrary(NVDA_MODULE);
 			if (this->module) {
@@ -29,74 +23,33 @@ ScreenReaderNVDA::~ScreenReaderNVDA() {
 			if (this->loaded && !this->Is_Active) {
 				this->is_running();
 			}
-		}
-		#else
-		this->loaded = true;
-		if(!this->Is_Active) {
-				this->is_running();
-		}
-		#endif
+}
 	}
 	void ScreenReaderNVDA::release() {
-		#ifndef SPEECH_CORE_STATIC_SCREEN_READER_APIS
 		if (this->module) {
 			this->loaded = false;
+			this->Is_Active = false;
 			FreeLibrary(this->module);
 			this->module = NULL;
 		}
-		#endif
-		this->Is_Active = false;
 	}
 	bool ScreenReaderNVDA::is_running() {
-	return this->Controller_testIfRunning();
-	}
-	bool ScreenReaderNVDA::speak_text(const wchar_t* text,bool interrupt) {
-	if(interrupt && !this->Controller_cancelSpeech()) {
+		if (this->module) {
+			return (nvdaController_testIfRunning_fn() == 0) ? true : false;
+}
 		return false;
 	}
-	auto state = this->Controller_speakText(text);
-	return state;
-	}
-	bool ScreenReaderNVDA::stop_speech() {
-	return this->Controller_cancelSpeech();
-}
-#ifndef SPEECH_CORE_STATIC_SCREEN_READER_APIS
-bool ScreenReaderNVDA::Controller_testIfRunning() {
-	if (this->module) {
-		return (nvdaController_testIfRunning_fn() == 0) ? true : false;
-	}
-	return false;
-}
-
-	bool ScreenReaderNVDA::Controller_speakText(const wchar_t* text) {
+	bool ScreenReaderNVDA::speak_text(const wchar_t* text,bool interrupt) {
 if (this->module) {
-	auto state = nvdaController_speakText_fn(text);
+	auto state = nvdaController_speakText_fn(text,interrupt);
 	return (state == 0) ? true : false;
 		}
 		return false;
 	}
-
-
-bool ScreenReaderNVDA::Controller_cancelSpeech() {
-	if (this->module) {
-		return (nvdaController_cancelSpeech_fn() == 0) ? true : false;
-	}
-	return false;
+	bool ScreenReaderNVDA::stop_speech() {
+		if (this->module) {
+			auto state = nvdaController_cancelSpeech_fn();
+				return (state == 0) ? true : false;
+		}
+		return false;
 }
-
-
-#else
-bool ScreenReaderNVDA::Controller_testIfRunning() {
-	return (nvdaController_testIfRunning() == 0) ? true : false;
-}
-
-	bool ScreenReaderNVDA::Controller_speakText(const wchar_t* text) {
-	auto state = nvdaController_speakText(text);
-	return (state == 0) ? true : false;
-	}
-
-bool ScreenReaderNVDA::Controller_cancelSpeech() {
-	return (nvdaController_cancelSpeech() == 0) ? true : false;
-}
-
-#endif
--- src/SCDrivers/nvda.h
+++ src/SCDrivers/nvda.h
@@ -7,27 +7,19 @@
 #else
 #define NVDA_MODULE L"nvdaControllerClient32.dll"
 #endif
-#ifndef SPEECH_CORE_STATIC_SCREEN_READER_APIS
+
 typedef error_status_t(__stdcall* NvdaController_testIfRunning_t)(void);
-typedef error_status_t(__stdcall* NvdaController_speakText_t)(const wchar_t* text);
+typedef error_status_t(__stdcall* NvdaController_speakText_t)(const wchar_t* text,bool interrupt);
 typedef error_status_t(__stdcall* NvdaController_cancelSpeech_t)(void);
-#endif
 
 class ScreenReaderNVDA : public ScreenReader {
 private:
+	HMODULE module;
 	bool loaded;
 	bool Is_Active;
-#ifndef SPEECH_CORE_STATIC_SCREEN_READER_APIS
-	HMODULE module;
 	NvdaController_testIfRunning_t nvdaController_testIfRunning_fn;
 	NvdaController_speakText_t nvdaController_speakText_fn;
 	NvdaController_cancelSpeech_t nvdaController_cancelSpeech_fn;
-	#endif
-	// if we're statically linking, these directly call NVDA controller. If we're not, this tries to call the loaded DLL and returns false if not loaded
-	bool Controller_testIfRunning();
-	bool Controller_speakText(const wchar_t* text);
-	bool Controller_cancelSpeech();
-
 public:
 	ScreenReaderNVDA();
 	~ScreenReaderNVDA();
